c27906d2d757537293c597cf33c609ff3ae36415
Adam Vigas <adamvigas1@gmail.com>
2024-09-13 15:30:04 +0200

    Updating deploy script

--- infra/scripts/deploy.bat
+++ infra/scripts/deploy.bat
@@ -10,13 +10,25 @@ kubectl apply -f .\k8s\service.yaml
 kubectl apply -f .\k8s\hpa.yaml
 kubectl apply -f .\k8s\pdb.yaml
 
-:: Wait for the pods to be ready
+:: Wait for all pods to be ready
 :wait_for_pods
-kubectl get pods | findstr "1/1"
-IF %ERRORLEVEL% NEQ 0 (
+set /a ready_count=0
+set /a total_count=0
+
+for /f "tokens=1,2" %%a in ('kubectl get pods --no-headers ^| findstr /r /c:"[0-9]/[0-9]"') do (
+    set /a total_count+=1
+    if "%%b"=="1/1" set /a ready_count+=1
+)
+
+if "%ready_count%"=="%total_count%" (
+    echo All pods are ready.
+    goto :continue
+) else (
+    echo Waiting for pods to be ready...
     timeout /t 5 /nobreak
     goto wait_for_pods
 )
 
+:continue
 :: Open the Nginx service
 minikube service nginx-service
