6248183f9a184331d1c748d0d317049180143e44
Adam Vigas <adamvigas1@gmail.com>
2024-09-13 15:14:01 +0200

    Updated deploy script

--- infra/scripts/deploy.bat
+++ infra/scripts/deploy.bat
@@ -1,8 +1,22 @@
+@echo off
+:: Start Minikube
 minikube start
+
+:: Apply Kubernetes manifests
 kubectl apply -f .\k8s\service-account.yaml
 kubectl apply -f .\k8s\secret.yaml
 kubectl apply -f .\k8s\deployment.yaml
 kubectl apply -f .\k8s\service.yaml
 kubectl apply -f .\k8s\hpa.yaml
 kubectl apply -f .\k8s\pdb.yaml
+
+:: Wait for the pods to be ready
+:wait_for_pods
+kubectl get pods | findstr "1/1"
+IF %ERRORLEVEL% NEQ 0 (
+    timeout /t 5 /nobreak
+    goto wait_for_pods
+)
+
+:: Open the Nginx service
 minikube service nginx-service
